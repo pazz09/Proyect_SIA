62f4637e34ea5f72937e8d3cb88b36b188d9039d
Estebanc0 <esteban.carrasco.o@mail.pucv.cl>
2023-03-26 20:09:27 -0300

    se crean set y get

--- src/usuarios.java
+++ src/usuarios.java
@@ -1,4 +1,68 @@
 
 public class usuarios {
 	private String nombre;
+	private int edad;
+	private String genero;
+	private String correo;
+	private String rut;
+	private int exp;
+	private String titulo;
+	private boolean posee_Titulo;
+	
+	public void set_expt(int a) {
+		this.exp=a;
+	}
+	
+	public int get_expt() {
+		return exp;
+	}
+	public void set_edad(int a) {
+		this.edad=a;
+	}
+	
+	public int get_edad() {
+		return edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String a) {
+		this.nombre = a;
+	}
+	
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String a) {
+		this.genero = a;
+	}
+	
+	public String getcorreo() {
+		return correo;
+	}
+	public void setcorreo(String a) {
+		this.correo = a;
+	}
+	
+	public String getrut() {
+		return rut;
+	}
+	public void setrut(String a) {
+		this.rut = a;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String a) {
+		this.titulo = a;
+	}
+	public boolean getposee() {
+		return posee_Titulo;
+	}
+	public void setposee(boolean a) {
+		this.posee_Titulo = a;
+	}
+	
+	
 }
