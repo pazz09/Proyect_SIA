a084d9a6d80575774fc961e4010ebefbdb8e3d99
Estebanc0 <esteban.carrasco.o@mail.pucv.cl>
2023-03-26 22:53:49 -0300

    agregan métodos y constructor a solicitudes

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.util.List;
 
 public class main {
 	public static void main(String[] arg) {
--- src/solicitudes.java
+++ src/solicitudes.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class solicitudes {
-
+	private List<empresa> ofertasT;
+	private List<usuarios> puesto;
+	
+	
+	public solicitudes(){
+		ofertasT=new ArrayList<>();
+		puesto= new ArrayList<>();
+	}
+	public void agregarOfertaE(empresa oferta) {
+        ofertasT.add(oferta);
+    }
+	public void agregarOfertaT(usuarios oferta) {
+        puesto.add(oferta);
+    }
 }
