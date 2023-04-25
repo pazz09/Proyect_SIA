eb3d31c522d3124c5bf2f4d3a0fbd214bdeae2a4
Estebanc0 <esteban.carrasco.o@mail.pucv.cl>
2023-04-25 18:20:34 -0400

    se implementa try-catch

--- src/Trabajo.java
+++ src/Trabajo.java
@@ -86,79 +86,96 @@ public class Trabajo {
 
 
 	public void manipularPostulantes(Usuario a) {
-        Scanner input = new Scanner(System.in);
-        
-        //Se comprueba que existan vacantes disponibles
-        if(this.vacantes<=0) {
-        	
-        	System.out.println("No hay vacantes disponibles");
-        	return;
-        }
-        //En caso de existir vacantes se comprueba la experiencia nesesaria
-        
-        //este if comprueba si no se requiere titulo
-        if(this.titulo==null || this.titulo.contentEquals("No posee")) {
-        	
-        	//comprueban la experiencia nesesaria
-            if(a.getExp()>=this.exp || this.exp==0 ) {
-                this.listPostulantes.add(a);
-                System.out.println("Postulacion exitosa");
-                this.vacantes-=1;
-                return;
-            }
-            if (a.getExp()<this.exp ) {
-                System.out.println("No posees la experiencia requerida ¿Aun quiere continuar?(si/no)");
-                String aux=input.nextLine();
-                if(aux.contentEquals("si"));{
-                    this.listPostulantes.add(a);
-                    System.out.println("Postulacion exitosa");
-                    this.vacantes-=1;
-					return;
-                }
+	    Scanner input = new Scanner(System.in);
+	    try {
+	        //Se comprueba que existan vacantes disponibles
+	        if (this.vacantes <= 0) {
+	            System.out.println("No hay vacantes disponibles");
+	            return;
+	        }
 
-            }
-        }
-        //Comprueba si se poseee el titulo nesesario
-        if(a.getTitulo().contentEquals(this.titulo)) {
-            if(a.getExp()>=this.exp) {
-                this.listPostulantes.add(a);
-                System.out.println("Postulacion exitosa");
-                this.vacantes-=1;
-					return;
-            }
-            else {
-                System.out.println("No posees la experiencia requerida ¿Aun quiere continuar?(si/no)");
-                String aux=input.nextLine();
-                if(aux.contentEquals("si"));{
-                    this.listPostulantes.add(a);
-                    System.out.println("Postulacion exitosa");
-                    this.vacantes-=1;
-					return;
-                }
-            }
-        }
-        //en caso de no poseer el titulo nesesario para el trabajo se manda el siguiente mensaje
-        else {
-            System.out.println("No posees el titulo nesesario para postular a este trabajo");
-            return;
-        }
+	        //En caso de existir vacantes se comprueba la experiencia necesaria
 
-    }
-	public void Mostrar_postulantes() {
-		for (int i=0;i<this.listPostulantes.size();i++) {
-			System.out.println(this.listPostulantes.get(i).getNombre()+"\n");
-		}
+	        //este if comprueba si no se requiere titulo
+	        if (this.titulo == null || this.titulo.equals("No posee")) {
+	            // comprueba si se requiere experiencia 
+	            if (a.getExp() >= this.exp || this.exp == 0) {
+	                this.listPostulantes.add(a);
+	                System.out.println("Postulacion exitosa");
+	                this.vacantes--;
+	                return;
+	            } else {
+	                System.out.println("No posees la experiencia requerida ¿Aun quieres continuar? (si/no)");
+	                String aux = input.nextLine();
+	                if (aux.equals("si")) {
+	                    this.listPostulantes.add(a);
+	                    System.out.println("Postulacion exitosa");
+	                    this.vacantes--;
+	                    return;
+	                } else {
+	                    System.out.println("Postulacion cancelada");
+	                    return;
+	                }
+	            }
+	        }
+	        //Comprueba si se posee el titulo necesario
+	        if (a.getTitulo().contentEquals(this.titulo)) {
+	            if (a.getExp() >= this.exp) {
+	                this.listPostulantes.add(a);
+	                System.out.println("Postulacion exitosa");
+	                this.vacantes -= 1;
+	                return;
+	            } else {
+	                System.out.println("No posees la experiencia requerida ¿Aun quieres continuar? (si/no)");
+	                String aux = input.nextLine();
+	                if (aux.equals("si")) {
+	                    this.listPostulantes.add(a);
+	                    System.out.println("Postulacion exitosa");
+	                    this.vacantes--;
+	                    return;
+	                } else {
+	                    System.out.println("Postulacion cancelada");
+	                    return;
+	                }
+	            }
+	        }
+	        //en caso de no poseer el titulo necesario para el trabajo se manda el siguiente mensaje
+	        else {
+	            System.out.println("No posees el titulo necesario para postular a este trabajo");
+	            return;
+	        }
+	    } catch (Exception e) {
+	        System.out.println("Ha ocurrido un error: " + e.getMessage());
+	        e.printStackTrace();
+	    }
 	}
 	public void manipularPostulantes(String nombre) {
-        for (int i=0;i<this.listPostulantes.size();i++) {
-            if(this.listPostulantes.get(i).getNombre().contentEquals(nombre)) {
-                this.listPostulantes.remove(i);
-                System.out.println("postulacion removida");
-            }
-        }
-
-    }
-	
+	    try {
+	        boolean found = false;
+	        for (int i = 0; i < this.listPostulantes.size(); i++) {
+	            if (this.listPostulantes.get(i).getNombre().contentEquals(nombre)) {
+	                this.listPostulantes.remove(i);
+	                System.out.println("Postulacion removida");
+	                found = true;
+	                break;
+	            }
+	        }
+	        if (!found) {
+	            throw new Exception("No se encontró ninguna postulación con el nombre proporcionado.");
+	        }
+	    } catch (Exception e) {
+	        System.out.println(e.getMessage());
+	    }
+	}
+	public void mostrarPostulantes() {
+	    try {
+	        for (int i = 0; i < this.listPostulantes.size(); i++) {
+	            System.out.println(this.listPostulantes.get(i).getNombre() + "\n");
+	        }
+	    } catch (Exception e) {
+	        System.out.println("Error al mostrar los postulantes: " + e.getMessage());
+	    }
+	}
 	public void buscarPostulante(String n) {
 		for (int i=0;i<this.listPostulantes.size();i++) {
 			if(this.listPostulantes.get(i).getNombre().contentEquals(n)) {
